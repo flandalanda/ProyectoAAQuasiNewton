6c81e82f7d5fca6593cc024eed2065cd8ab82b12
Rafael <r4f4al@gmail.com>
2020-05-06 20:48:37 -0500

    idea del 2_3

--- ej2_3.m
+++ ej2_3.m
@@ -4,7 +4,14 @@ clear;   close all;   clc;
 %Define la función DIXMAANA
 f=@(x) dixmaana(x);
 
-% n en {240,960,3,9,15,51,87} 
+% n en {240,960}
+% m en {1,3,5,17,29}
 
-x0 = 2*ones([n,1]);
+for n in [240,960]
+	x0 = 2*ones([n,1]);
 
+	for m in [1,3,5,17,29]
+		sol() = lineLM_BFGS(f,x0,m,tol,maxiter)
+
+	end
+end
