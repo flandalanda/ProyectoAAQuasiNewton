1bf33b92c05442ebdf2a983a7f3d266406654580
Uriel <60166735+peanocurve@users.noreply.github.com>
2020-05-07 15:33:33 -0500

    correccion linesearch

--- lineSearch.m
+++ lineSearch.m
@@ -10,7 +10,7 @@ function [alpha, gnew] = lineSearch( f, xk, dk, gk )
 % We define global constants
 c1 = 10e-4;
 c2 = 0.99;
-phiPrime0 = dot(dk,gk);
+phiPrime0 = dk'*gk;
 phi0 = f(xk);
 maxIter = 1000;
 
@@ -28,22 +28,22 @@ while(iter < maxIter)
     % We approximate the gradient at the new proposed point and calculate
     % the derivative with respect to alpha at said point
     gnew = apGrad(f, xk + alpha*dk);
-    phiPrimeA = dot(gnew, dk);
+    phiPrimeA = gnew'*dk;
     
     % We check if any conditions have been violated
-    if phiA > phi0 + c1*a1*phiPrime0 || (phiA >= phi0 && iter >1)
-        [alpha, gnew] = zoom( a0, alpha, f, xk, fk, gTd);
+    if phiA > phi0 + c1*a0*phiPrime0 || (phiA >= phi0 && iter >1)
+        [alpha, gnew] = zoom( a0, alpha, f, xk,dk, phi0, phiPrime0);
         break
-    elseif abs(phiPrime1) <= -c2*phiPrime0
+    elseif abs(phiPrimeA) <= -c2*phiPrime0
         break
-    elseif phiPrime1 >= 0
-        [alpha, gnew] = zoom(alpha, a0, f, xk, dk, fk, phiPrime0);
+    elseif phiPrimeA >= 0
+        [alpha, gnew] = zoom(alpha, a0, f, xk, dk, phi0, phiPrime0);
         break;
     else
         % We refine the interval
         a0 = alpha;
         phi0 = phiA;
-        i = i + 1;
+        iter = iter + 1;
     end
 end
 
@@ -65,10 +65,10 @@ function [alpha, gnew] = zoom(alo, ahi, f, xk, dk, phi0, phiPrime0)
        alpha = 0.5*(alo+ahi);
        phiA = f(xk+ alpha*dk);
        gnew = apGrad(f, xk + alpha*dk);
-       phiPrime1 = dot(gnew, dk);
+       phiPrime1 = gnew'*dk;
        
        % We check if any condition is not met
-       if(phiA > phi0 + alpha*c1*phiPrime0) || (phiA >=phiLo)
+       if((phiA > phi0 + alpha*c1*phiPrime0) || (phiA >=phiLo))
            % If the step was too long, we refine the interval 
            ahi = alpha;
            % If W2 is met, we found a correct alpha
@@ -79,7 +79,7 @@ function [alpha, gnew] = zoom(alo, ahi, f, xk, dk, phi0, phiPrime0)
            if phiPrime1 * (ahi-alo)>=0
                ahi = alo;
            end
-           al = alpha;
+           alo = alpha;
            iter = iter+1;
        end
     end
