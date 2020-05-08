c38822ed3641389d1a0d4b26d9d5b3802a3295aa
MarcoMedS <mamedinasalgado@gmail.com>
2020-05-07 19:49:03 -0500

    Update ej2_3

--- ej2_3.m
+++ ej2_3.m
@@ -1,17 +1,27 @@
 % Script to perform excercise 2.3
 clear;   close all;   clc;
 
-%Define la función DIXMAANA
+% Handle for DIXMAANA function
 f=@(x) dixmaana(x);
 
-% n en {240,960}
-% m en {1,3,5,17,29}
+% Parameters n (DIXMAANA) and m (LM BFGS)
+n = [240,960];
+m = [1,3,5,17,29];
 
-for n in [240,960]
-	x0 = 2*ones([n,1]);
+% Aux variables
+norma = [];
+valor = [];
+tiempo = [];
+iter = [];
 
-	for m in [1,3,5,17,29]
-		sol() = lineLM_BFGS(f,x0,m,tol,maxiter)
-
+for i = 1:length(n) 
+	x0 = 2*ones([n(i),1]);
+	for j = 1:length(m)
+        tic
+		[xk, k] = lineLM_BFGS(f, x0, 10^(-5), 1000, m(j));
+        tiempo = [tiempo, toc];        
+        norma = [norma, norm(apGrad(f,xk))];
+        valor = [valor, f(xk)];
+        iter = [iter, k];
 	end
 end
